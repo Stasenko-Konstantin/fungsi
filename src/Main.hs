676b36427eea942c15b22d180ad52ffe6841bfde
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2021-08-10 14:04:38 +0700

    repl

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import System.IO
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    putStr "< "
+    hFlush stdout
+    line <- getLine
+    if line == ":q" 
+       then return ()
+       else do
+           putStr "> "
+           putStrLn line
