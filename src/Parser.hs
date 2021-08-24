6d3e63011cc0fde2d98fae1fa92c4a753ad422da
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2021-08-24 21:05:42 +0700

    start parse

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,6 +4,16 @@ import Token
 import Help
 
 parse :: [Token] -> Expr
-parse tokens@(t:okens) = start tokens
+parse tokens@(t:okens) = case t of
+	{_, _, "Num"}  -> parseNum
+	{_, _, "Name"} -> parseName
+	{_, "(", _}    -> parseGroup
+	{_, "@", _}    -> parseLambda
+	{_, "-", _}    -> parseUnaryMinus
+	{_, _, "None"} -> error "Parse error"
 	where
-		start (Token token content subtype) = undefined
+		parseNum        = undefined
+		parseName       = undefined
+		parseGroup      = undefined
+		parseLambda     = undefined
+		parseUnaryMinus = undefined
